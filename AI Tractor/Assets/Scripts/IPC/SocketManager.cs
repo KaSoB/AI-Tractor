e19999c44691763984dcd478049f303265a516b0
m1kra <mieczyslaw.krawiarz@gmail.com>
2018-09-01 22:00:01 +0200

    add python server

---
+++ AI Tractor/Assets/Scripts/IPC/SocketManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+
+public class SocketManager
+{
+	static string HOST = "127.0.0.1";
+	static int PORT = 8000;
+
+	System.Net.Sockets.TcpClient socket;
+	System.IO.Stream stream;
+	System.IO.StreamReader reader;
+	System.IO.StreamWriter writer;
+
+	public SocketManager ()
+	{
+		this.socket = new System.Net.Sockets.TcpClient(HOST, PORT);
+		this.stream = this.socket.GetStream ();
+		this.reader = new System.IO.StreamReader (stream);
+		this.writer = new System.IO.StreamWriter (stream);
+	}
+
+	public void GetInfo(String msg){
+		this.writer.Write(msg + "\n");
+		this.writer.Flush ();
+		var res = this.reader.ReadLine ();
+		Debug.Log (res);
+	}
+}
+
