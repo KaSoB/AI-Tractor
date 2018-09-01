66a558936f5541d1daac69db061fd540f6365079
m1kra <mieczyslaw.krawiarz@gmail.com>
2018-09-01 22:00:01 +0200

    add python server

---
+++ AI Tractor/server.py
@@ -0,0 +1,43 @@
+import sys
+import socket
+import asyncio
+
+
+HOST = '127.0.0.1'
+PORT = 8000
+
+async def read_data(reader):
+    data = b''
+    while True:
+        chunk = await reader.read(64)
+        if not chunk or chunk == b'':
+            break
+        data += chunk
+    return data
+
+
+async def handle_client(reader, writer):
+    print("connected")
+    while True:
+        data = await reader.readline(timeout=1)
+        if not data:
+            print("disconnectng")
+            break
+        print(data)
+        result = "some_text\n"
+        writer.write(str(result).encode())
+
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    coro = asyncio.start_server(handle_client, HOST, PORT, loop=loop)
+    server = loop.run_until_complete(coro)
+
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        pass
+
+    server.close()
+    loop.run_until_complete(server.wait_closed())
+    loop.close()
